ac268a29e5a2c02128ad2f5dcb6855f03838755a
Davi Alvarenga <davialvarenga@hotmail.com.br>
2024-07-10 21:01:51 -0300

    feat: implementação da função da soma dos quadrados

--- Exercism/C/difference-of-squares/difference_of_squares.c
+++ Exercism/C/difference-of-squares/difference_of_squares.c
@@ -1,20 +1,33 @@
 #include "difference_of_squares.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-int numeros(int number){
-
-int* naturalNumbers = malloc(number * sizeof(int));
-
-for(int i = 0; i < number; i++){
-naturalNumbers[i] = i + 1;
-printf("naturalNumbers[%d] = %d\n", i, naturalNumbers[i]);
+int* generateNaturalNumbers(int number) {   // Função que gera valores naturais em ordem crescente
+  int* naturalNumbers = malloc(number * sizeof(int));
+  for (int i = 0; i < number; i++) {    // Loop para colocar os números de ordem crescente em um array
+    naturalNumbers[i] = i + 1;
+  }
+  return naturalNumbers;
 }
 
-return 1; 
-
+int sumArray(int *arr, int size) {   // Define a função sumArray que recebe um ponteiro para um array de inteiros (arr).
+  int sum = 0;
+  for (int i = 0; i < size; i++) { // Avança o ponteiro para o próximo elemento
+    sum += arr[i];
+  }
+  return sum;
 }
 
-int main(){
-    numeros(10);
+int square_of_sum(unsigned int number) {
+  int* naturalNumbers = generateNaturalNumbers(number);
+  int sum = sumArray(naturalNumbers, number);
+  float square_of_sum = (float)sum * (float)sum; // Converta sum para float antes de calcular o quadrado
+  free(naturalNumbers);
+  return square_of_sum;
 }
+
+
+
+
+
+
+  
