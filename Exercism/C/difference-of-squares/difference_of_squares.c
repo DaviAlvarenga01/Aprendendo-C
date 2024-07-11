f0c9a0c5b936359699576114a67217ce5744aea7
Davi Alvarenga <davialvarenga@hotmail.com.br>
2024-07-11 12:58:39 -0300

    feat: adicao de um novo exercicio

--- Exercism/C/difference-of-squares/difference_of_squares.c
+++ Exercism/C/difference-of-squares/difference_of_squares.c
@@ -1,5 +1,6 @@
 #include "difference_of_squares.h"
 
+
 unsigned int sum_of_squares(unsigned int number) {
     return number * (number + 1) * (2 * number + 1) / 6;
 }
@@ -13,4 +14,8 @@ unsigned int difference_of_squares(unsigned int number) {
     return square_of_sum(number) - sum_of_squares(number);
 }
 
-
+int main (){
+    square_of_sum(10);
+    sum_of_squares(5);
+    difference_of_squares(10);
+}
